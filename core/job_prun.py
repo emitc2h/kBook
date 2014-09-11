cac96cca292df44e58ef51fc7f01d4c1d5fa87d5
Michel Trottier-McDonald <emitc2h@gmail.com>
2014-09-12 00:06:24 +0200

    Minor style issues

--- core/job_prun.py
+++ core/job_prun.py
@@ -33,8 +33,8 @@ class JobPrun(Job):
 
 		self.type         = 'prun'
 
-		self.legend_string = 'index : name                 : type         : script_name          : version'
-		self.ls_pattern    = ('{0:<5} : {1:<20} : {2:<12} : {3:<20} : {4:<5}', 'index', 'name', 'type', 'script_name', 'version')
+		self.legend_string = 'index : type         : script_name          : version'
+		self.ls_pattern    = ('{0:<5} : {2:<12} : {3:<20} : {4:<5}', 'index', 'type', 'script_name', 'version')
 
 
 	## --------------------------------------------------------
